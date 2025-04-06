3e8cc5a31bbc10c0065afb0ef84e87934539d66d
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2025-04-06 10:32:49 +0300

    finalizing welcome page and add some config related to font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
-import { Inter_900Black, useFonts } from "@expo-google-fonts/inter";
+import { Inter_900Black } from "@expo-google-fonts/inter";
+import {
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium
+} from "@expo-google-fonts/poppins";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     Inter_900Black,
+    Poppins_400Regular, // Regular weight
+    Poppins_700Bold,
+    Poppins_500Medium
+     // Bold weight
   });
 
   useEffect(() => {
@@ -16,6 +27,7 @@ export default function RootLayout() {
   if (!loaded && !error) {
     return null;
   }
+
   return (
     <Stack>
       <Stack.Screen name="(auth)" />
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-
-
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Link, router } from "expo-router";
 export default function Index() {
   return (
     <View style={style.container}>
@@ -8,8 +9,8 @@ export default function Index() {
         <View
           style={{
             padding: 2,
-            backgroundColor: "white",
-            borderRadius: 10,
+            backgroundColor: "#9CFCE3",
+            borderRadius: 50,
           }}
         >
           <Image
@@ -18,41 +19,149 @@ export default function Index() {
           />
         </View>
       </View>
-      <View style={style.image}>
-        <Image
-          source={require("../assets/images/landing.png")}
-          style={{ width: 300, height: 300 }}
-        />
+      <View>
+        <Text style={style.text}>
+          Welcome to {"\n"}
+          <Text style={style.textspan}>Moodizer AI</Text>
+        </Text>
+      </View>
+      <View>
+        <Text style={style.description}>
+          A smarter way to track and improve your emotional well-being —
+          anytime, anywhere 🌿
+        </Text>
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 10,
+          marginTop: 10,
+          // borderColor: "#06D6A0",
+          // borderWidth: 2,
+        }}
+      >
+        <View>
+          <Image
+            source={require("../assets/images/landing.png")}
+            style={{ width: 300, height: 300 }}
+            resizeMode="cover"
+          />
+        </View>
+      </View>
+      <View
+        style={{
+         justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={style.description}>
+          By signing up, you agree to our{" "}
+          <Text style={{ color: "#06D6A0" }}>Terms of Service</Text> and{" "}
+          <Text style={{ color: "#06D6A0" }}>Privacy Policy</Text>
+        </Text>
+        <View>
+          <TouchableOpacity
+            style={style.button}
+            onPress={() => {
+              router.replace("/signin");
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontSize: 18,
+                fontFamily: "Poppins_700Bold",
+              }}
+            >
+              Get Started
+            </Text>
+            <AntDesign name="arrowright" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+        <View style={{ padding: 8,marginBlock:8 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              color: "#155451",
+              fontFamily: "Poppins_400Regular",
+            }}
+          >
+            Already have an account?{" "}
+            <Text
+              style={{ color: "#06D6A0" }}
+              onPress={() => router.replace("/login")}
+            >
+              Sign In
+            </Text>
+          </Text>
+        </View>
       </View>
     </View>
   );
 }
 const style = StyleSheet.create({
   container: {
+    paddingBlock: 20,
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#f9f4f4",
+    backgroundColor: "white",
   },
-  image: {
-    marginTop: 70,
+  moreinfo: {
+    marginTop: 20,
+    padding: 8,
+    gap: 14,
   },
+  infotext: {
+    fontSize: 20,
+    color: "#185855",
+    fontFamily: "Poppins_500Medium",
+  },
+
   text: {
-    fontSize: 30,
-    fontWeight: "bold",
+    fontSize: 32,
+    gap: 8,
+    marginTop: 10,
+    color: "#155554",
     padding: 4,
+    lineHeight: 30,
     fontFamily: "Inter_900Black",
+    textAlign: "center",
   },
   textspan: {
     color: "green",
+    fontSize: 25,
+  },
+  mood: {
+    fontFamily: "Poppins_700Bold",
+    color: "#06D6A0",
+    borderColor: "#07D6A0",
+    borderWidth: 2,
+  },
+  description: {
+    textAlign: "center",
+    padding: 8,
+    fontSize: 16,
+    color: "#155451",
+    fontFamily: "Poppins_400Regular",
+    lineHeight: 24,
+  },
+  button: {
+    flexDirection: "row",
+    backgroundColor: "#06D6A0",
+    paddingBlock: 12,
+    width: 200,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
   },
   header: {
-    marginTop: 20,
+    marginTop: 10,
     flexDirection: "row",
     padding: 8,
     justifyContent: "center",
     alignItems: "center",
-    // borderColor: "green",
-    // borderWidth: 1,
     gap: 10,
   },
 });
